bcfa7696c0f00858134020e64e90d14504841a5f
Dmitriy Kukharenko <dmitriy.kukharenko@duxteam.com>
2017-04-26 16:43:24 +0300

    working in debug

---
+++ src/ForestFires/Models/WeatherData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForestFires.Models
+{
+    public class WeatherData
+    {
+        /// <summary>
+        /// класс-контейнер для хранения полученных данных
+        /// </summary>        
+        public int AirTemperature { get; set; }
+        public double Precipitation { get; set; }
+        public double Pressure { get; set; }
+        public string WindSpeed { get; set; }
+        public string Humidity { get; set; }
+        public string City { get; set; }
+        public string WeatherType { get; set; }
+        public string Date { get; set; }
+    }
+}
